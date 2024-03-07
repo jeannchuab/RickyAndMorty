c6b8af7ec981041d89a33a4ee5ca3d124e1c2ae3
Jeann Chuab <jeann.chuab@gmail.com>
2024-03-07 09:31:59 -0300

    Initial project setup

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Jeann Luiz on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
